c71645eaefe2575d9101053a2d6d6c40fc9bd2a2
Erzan <ramond_lawencon>
2023-07-19 19:54:47 +0700

    namabahin menu login

---
+++ views/login/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate, numquam aliquam dolore voluptatibus necessitatibus odio perferendis repellat eligendi non. Reprehenderit quos velit commodi! Velit in ullam officiis illo impedit?</p>
+</body>
+
+</html>
