76b9dbe3a794bfb7f6fa5d0286b41362f0e8ced1
Erzan <ramond_lawencon>
2023-07-22 15:29:45 +0700

    unsued func

--- config/Application.php
+++ config/Application.php
@@ -20,13 +20,6 @@ class Application
      * @param
      */
 
-    function getServer() {
-        echo $_SERVER['REQUEST_URI'];    }
-
-        public $new = function() {
-            
-        };
-
     public function run()
     {
         // ini_set('error_reporting', E_ALL);
